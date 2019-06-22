1c31b3c68fae0663a8148a9d6721764f219fca86
Ibrahim Olayinka <ibrahimolayinkaa@gmail.com>
2019-06-22 03:44:41 +0100

    another commit

--- src/components/Pop.vue
+++ src/components/Pop.vue
@@ -20,20 +20,26 @@
           class="headline grey lighten-2"
           primary-title
         >
-          Privacy Policy
+          ADD A NEW PROJECT
         </v-card-title>
 
         <v-card-text>
-            <v-form>
-                <v-text-field label="Title" solo></v-text-field>
+            <v-form ref="form">
+                <v-text-field label="Title" v-model="project.title" solo prepend-icon="folder" :rules="inputRules"></v-text-field>
                 <v-textarea 
                 label="Information" 
                 solo
                 rows="3"
+                v-model="project.content" prepend-icon="edit"
             
                 >
                 </v-textarea>
-                <v-btn class="blue lighten-1 white--text">
+                <v-menu>
+                  <v-text-field :value="formattedDate" slot="activator" label="Due Date" prepend-icon="date_range"></v-text-field>
+                  <v-date-picker v-model="project.due"></v-date-picker>
+                </v-menu>
+                <v-spacer></v-spacer>
+                <v-btn class="blue lighten-1 white--text"@click="submit">
                     <v-icon>done</v-icon>
                     Add Project
                 </v-btn>
@@ -52,6 +58,7 @@
 </template>
 
 <script>
+import format from 'date-fns/format'
     export default {
 
         name:'Pop',
@@ -59,8 +66,45 @@
 
             return{
 
-                dialog:false
+                dialog:false,
+                project: {
+                  
+                    title:'',
+                    content:'',
+                    due:null
+
+                },
+                inputRules: [
+
+                  v => !!v || 'Name is required',
+                  v => /^[a-zA-Z]+$/.test(v) || 'Only Alphabet are allowed',
+                  v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+
+                ],
+                emailRules:[
+
+                    v => !!v || 'E-mail is required',
+                    v => /.+@.+/.test(v) || 'E-mail must be valid'
+                ]
             }
+        },
+        methods: {
+
+            submit(){
+
+                if(this.$refs.form.validate()){
+
+                  console.log(this.project.title)
+                }
+
+            }
+        },
+        computed: {
+
+          formattedDate(){
+
+                return this.project.due ? format(this.project.due,'DD/MM/YY'):''
+          }
         }
         
     }
